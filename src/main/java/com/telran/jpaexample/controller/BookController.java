18f0a197bf72f5cf6f6f7e9ee06485d0a2c14774
Alykirill <Alykirill@yandex.ru>
2019-07-30 12:24:54 +0300

    Initial commit

---
+++ src/main/java/com/telran/jpaexample/controller/BookController.java
@@ -0,0 +1,66 @@
+package com.telran.jpaexample.controller;
+
+
+import com.telran.jpaexample.repository.BookRepository;
+import com.telran.jpaexample.repository.entities.AuthorEntity;
+import com.telran.jpaexample.repository.entities.BookEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+
+public class BookController {
+    @Autowired
+    BookRepository repository;
+
+    @PostMapping("book")
+    public void addBook(@RequestBody BookEntity book){
+        repository.save(book);
+    }
+
+    @PostMapping("author")
+    public void addAuthor(@RequestBody AuthorDto authorDto) {
+        repository.addAuthor(authorDto.email,authorDto.name);
+    }
+
+    @GetMapping("book/all")
+    public List<BookDto> getAllBooks(){
+        return repository.getAllBooks().stream().map(this::toBookDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("book/all/{authorEmail}")
+    public List<BookDto> getBookByAuthor(@PathVariable("authorEmail")String authorEmail){
+        return repository.getAllBooksByAuthor(authorEmail).stream().map(this::toBookDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("{bookId}")
+    public BookDto getBookById(@PathVariable("bookId")Long bookId){
+        return toBookDto(repository.findById(bookId));
+    }
+
+    @DeleteMapping("{bookId}")
+    public void deleteBookById(@PathVariable("bookId")Long bookId){
+        repository.removeById(bookId);
+    }
+
+    private BookDto toBookDto(BookEntity entity) {
+        BookDto dto = new BookDto();
+        dto.id = entity.getId();
+        dto.title = entity.getTitle();
+        dto.authors = entity.getAuthors().stream().map(this::toAuthorDto).collect(Collectors.toList());
+        return dto;
+    }
+
+    private AuthorDto toAuthorDto(AuthorEntity entity) {
+        AuthorDto dto = new AuthorDto();
+        dto.email = entity.getEmail();
+        dto.name = entity.getName();
+        return dto;
+    }
+
+}
---
+++ src/main/java/com/telran/jpaexample/repository/BookRepositoryImpl.java
@@ -0,0 +1,96 @@
+package com.telran.jpaexample.repository;
+
+import com.telran.jpaexample.repository.entities.AuthorEntity;
+import com.telran.jpaexample.repository.entities.BookEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.swing.text.html.Option;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    @PersistenceContext
+    EntityManager em;
+
+
+
+
+    @Override
+    @Transactional
+    public boolean save(BookEntity book) {
+        List<AuthorEntity> list = new ArrayList<>();
+        for (AuthorEntity author : book.getAuthors()) {
+            AuthorEntity a = em.find(AuthorEntity.class, author.getEmail());
+            if (a == null) {
+                throw new RuntimeException("Author does not exist");
+            }
+            list.add(a);
+        }
+        book.setAuthors(null);
+        em.persist(book);
+        book.setAuthors(list);
+        return true;
+    }
+
+    @Override
+    @Transactional
+    public boolean removeById(long id) {
+        BookEntity res = em.find(BookEntity.class, id);
+        if (res != null) {
+            em.remove(res);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    @Transactional
+    public BookEntity findById(long id) {
+        return em.find(BookEntity.class, id);
+    }
+
+    @Override
+    @Transactional
+    public List<BookEntity> getAllBooks() {
+        return em.createQuery("select be from BookEntity AS be", BookEntity.class).getResultList();
+    }
+
+    @Override
+    @Transactional
+    public List<BookEntity> getAllBooksByAuthor(String authorEmail) {
+        TypedQuery<BookEntity> query = em.createQuery("select be from BookEntity be join be.authors a where a.email = :authorEmail", BookEntity.class);
+        query.setParameter("authorEmail", authorEmail);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void addAuthor(String email, String name) {
+        AuthorEntity authorEntity = em.find(AuthorEntity.class, email);
+        if (authorEntity == null) {
+            authorEntity = new AuthorEntity();
+            authorEntity.setEmail(email);
+            authorEntity.setName(name);
+            em.persist(authorEntity);
+            return;
+        }
+        throw new RuntimeException("Author already exist");
+    }
+
+
+//    getAllBooksByAuthor using @NamedQuery
+//    @Override
+//    public List<BookEntity> getAllBooksByAuthor(String authorEmail) {
+//        TypedQuery<BookEntity> query = em.createNamedQuery("getBooksByAuthor", BookEntity.class);
+//        query.setParameter("authorEmail", authorEmail);
+//        return query.getResultList();
+//    }
+
+
+}
